6b597e8768cc4c14b0ed0a5f90e5a7329d28f224
Jean-Baptiste Renaux <jb.renaux@me.com>
2020-06-12 16:42:52 +0200

    Init

---
+++ src/main/java/org/jibe77/hermanas/HermanasApplication.java
@@ -0,0 +1,13 @@
+package org.jibe77.hermanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermanasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermanasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jibe77/hermanas/HermanasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jibe77.hermanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermanasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
