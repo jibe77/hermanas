d2c35a2ac8b6951f85eda8b6015f8d9f4bf5a4a8
jibe77 <jb.renaux@me.com>
2020-08-24 23:15:54 +0200

    implement birdhouse button controller.

---
+++ src/main/java/org/jibe77/hermanas/gpio/birdhouse/BirdhouseButtonListener.java
@@ -0,0 +1,39 @@
+package org.jibe77.hermanas.gpio.birdhouse;
+
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import org.jibe77.hermanas.gpio.light.LightController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BirdhouseButtonListener implements GpioPinListenerDigital {
+
+    BirdhouseButtonController birdhouseButtonController;
+    LightController lightController;
+
+    Logger logger = LoggerFactory.getLogger(BirdhouseButtonListener.class);
+
+    public BirdhouseButtonListener(BirdhouseButtonController birdhouseButtonController, LightController lightController) {
+        this.birdhouseButtonController = birdhouseButtonController;
+        this.lightController = lightController;
+    }
+
+    @Override
+    public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+        if (event.getState().isHigh()) {
+            logger.info("Birdhouse button is pressed.");
+            if (lightController.isSwitchedOn()) {
+                birdhouseButtonController.setLightHasBeenSwitchedOnByBirdhouseDoor(false);
+            } else {
+                birdhouseButtonController.setLightHasBeenSwitchedOnByBirdhouseDoor(true);
+                lightController.switchOn();
+            }
+        } else if (event.getState().isLow()) {
+            logger.info("Birdhouse button is not pressed anymore.");
+            if (birdhouseButtonController.isLightHasBeenSwitchedOnByBirdhouseDoor()) {
+                lightController.switchOff();
+            }
+            birdhouseButtonController.setLightHasBeenSwitchedOnByBirdhouseDoor(false);
+        }
+    }
+}
