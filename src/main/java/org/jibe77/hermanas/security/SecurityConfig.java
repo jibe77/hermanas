af8b008a99b17244264498cff4376d47918bddc6
jibe77 <jb.renaux@me.com>
2020-08-31 22:10:51 +0200

    forgot a few annotations

--- src/main/java/org/jibe77/hermanas/security/SecurityConfig.java
+++ src/main/java/org/jibe77/hermanas/security/SecurityConfig.java
@@ -4,13 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableAutoConfiguration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
     Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
@@ -30,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/**").hasAnyRole("USER")
-                    .antMatchers(HttpMethod.GET, "/light/isSwitchedOn").hasAnyRole( "DEMO")
+                    .antMatchers(HttpMethod.GET, "/light/isSwitchedOn").hasRole( "DEMO")
                 .and()
                 .httpBasic();
     }
