c720fd38450cef5e4f759e3197b00a7afafba103
jibe77 <jb.renaux@me.com>
2021-02-26 03:11:09 +0100

    websocket

--- src/main/java/org/jibe77/hermanas/websocket/GreetingController.java
+++ src/main/java/org/jibe77/hermanas/websocket/GreetingController.java
@@ -1,5 +1,7 @@
 package org.jibe77.hermanas.websocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -10,9 +12,12 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 public class GreetingController {
 
+    Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
     @MessageMapping("/progress")
     @SendTo("/socket/progress")
     public Greeting websocketDemo(HelloMessage message) throws Exception {
+        logger.info("sending message from /process, send to /socket/progress.");
         Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
@@ -29,7 +34,8 @@ public class GreetingController {
             while (true) {
                 try {
                     Thread.sleep(3*1000); // Each 3 sec.
-                    websocketDemo(new HelloMessage("ceci est un test : " + i++));
+                    logger.info("publishing test ... ", i++);
+                    websocketDemo(new HelloMessage("ceci est un test : " + i));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
