2f625422ef1d086d230fcc85bfa58e1923e0fcca
jibe77 <jb.renaux@me.com>
2021-02-26 03:53:09 +0100

    websocket

--- src/main/java/org/jibe77/hermanas/websocket/WebSocketConfig.java
+++ src/main/java/org/jibe77/hermanas/websocket/WebSocketConfig.java
@@ -18,7 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket")
-                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
