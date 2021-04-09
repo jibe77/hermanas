06391263f63477255cfe9ad84ed8a6c8b990df36
jibe77 <jb.renaux@me.com>
2021-04-10 00:00:18 +0200

    picam loading so file

---
+++ src/test/java/org/jibe77/hermanas/controller/camera/CameraLoaderTest.java
@@ -0,0 +1,12 @@
+package org.jibe77.hermanas.controller.camera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+public class CameraLoaderTest {
+
+    @Test
+    void testBirdhouseIsOpenedAndLightIsAlreadyOn() {
+        Assert.isTrue(true, "");
+    }
+}
