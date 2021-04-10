89299bba5cbe0f66997513325343e60c256da772
jibe77 <jb.renaux@me.com>
2021-04-10 22:42:36 +0200

    cache

--- src/main/java/org/jibe77/hermanas/service/InfoService.java
+++ src/main/java/org/jibe77/hermanas/service/InfoService.java
@@ -1,6 +1,7 @@
 package org.jibe77.hermanas.service;
 
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class InfoService {
     }
 
     @GetMapping(value = "/info")
+    @Cacheable
     public BuildProperties version() {
         return buildProperties;
     }
