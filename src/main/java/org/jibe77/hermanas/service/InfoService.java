8f6b5654e390a0522b021de8628da5073626a4d2
jibe77 <jb.renaux@me.com>
2021-04-17 00:23:06 +0200

    remove cache

--- src/main/java/org/jibe77/hermanas/service/InfoService.java
+++ src/main/java/org/jibe77/hermanas/service/InfoService.java
@@ -1,5 +1,6 @@
 package org.jibe77.hermanas.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ public class InfoService {
 
     BuildProperties buildProperties;
 
+    @Autowired(required = false)
     public InfoService(BuildProperties buildProperties) {
         this.buildProperties = buildProperties;
     }
